d02a3f315435d00b894ba70020588426d1ec0eb3
Alejo Carballude <alejocarballude@gmail.com>
2023-03-12 19:10:05 -0300

    Add image class.

--- image.py
+++ image.py
@@ -1,20 +1,32 @@
-from PIL import Image as PILImage
+from PIL import Image as PILImage, ImageDraw as PILImageDraw
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+WHITE = (1,)
+BLACK = (0,)
 
 
 class Image:
-    def __init__(self, width, height, base_value=BLACK):
-        self.image = PILImage.Image(width, height, base_value)
+    def __init__(self, width, height, scale=10, base_value=BLACK):
+        self.scale = scale
+        self.image = PILImage.new('1', [width * scale, height * scale], base_value)
+        self.image_draw = PILImageDraw.Draw(self.image)
 
-    def set_pixel(self, x, y, value=WHITE):
-        self.image.set_pixel(x, y, value)
+    def _scale_value(self, value):
+        return value * self.scale
+
+    def _scale_iterable(self, iterable):
+        return [self._scale_value(i) for i in iterable]
+
+    def set_pixel(self, x, y, value=BLACK):
+        coords = self._scale_iterable([x, y, x+1, y+1])
+        self.image_draw.rectangle(coords, fill=value)
 
     def process_values(self, values):
         for y, line in enumerate(values):
             for x, value in enumerate(line):
                 self.set_pixel(x, y, value)
+
+    def show(self, path):
+        self.image.show(path)
 
     def save(self, path):
         self.image.save(path)
